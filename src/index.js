3f7a9b13cd7be5ff179462162fc6833bfcada594
Lucas Correia <lucascorreia-.-@live.com>
2020-07-03 11:06:10 -0300

    feat: added socket.io to recives messages

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const http = require('http');
+const socketIo = require('socket.io');
 
 const app = express();
 const httpServer = http.createServer(app);
+const io = socketIo(httpServer);
 
 app.get('/', (_req, res) => {
   res.sendFile(`${__dirname}/index.html`);
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
 });
 
 httpServer.listen(3000, () => {
