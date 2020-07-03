bd5f009a13da56daf87998d6bfd400e82e01f932
Lucas Correia <lucascorreia-.-@live.com>
2020-07-03 10:37:40 -0300

    style: config of prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,9 @@ module.exports = {
     es2020: true,
     node: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ['airbnb-base'],
   parserOptions: {
     ecmaVersion: 11,
   },
-  rules: {
-  },
+  rules: {},
 };
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-const express = require("express");
-const http = require("http");
+const express = require('express');
+const http = require('http');
 
 const app = express();
 const httpServer = http.createServer(app);
 
-app.get("/", (_req, res) => {
-  res.sendFile(__dirname + "/index.html");
+app.get('/', (_req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
 });
 
 httpServer.listen(3000, () => {
-  console.log("listening on *:3000");
+  console.log('listening on *:3000');
 });
